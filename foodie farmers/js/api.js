7d644fd06633d1dc529ae79c3b670448bfabf3f0
PureGero <puregero@gmail.com>
2020-09-17 11:15:35 +1000

    Add recommended items

--- foodie farmers/js/api.js
+++ foodie farmers/js/api.js
@@ -1,13 +1,27 @@
 'use strict'
 
+if ($('.recommend-items').length) {
+  $.get('/api/customer/list_recommend', data => {
+    data.forEach(item => {
+      $(`<div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
+        <a class="block-2-item" href="#">
+          <figure class="image" style="background-image: url('${item.image}')"></figure>
+          <div class="text">
+            <span class="text-uppercase">${item.farm}'s ${item.name}</span>
+            <h3>$${item.cost}</h3>
+          </div>
+        </a>
+      </div>`).appendTo('.recommend-items')
+    })
+  })
+}
+
 if ($('.group-purchase').length) {
   $.get('/api/customer/list_group_purchases', data => {
     data.forEach(item => {
       $(`<div class="item">
         <div class="block-4 text-center">
-          <figure class="block-4-image">
-            <img src="${item.image}" alt="Image placeholder" class="img-fluid">
-          </figure>
+          <figure class="block-4-image" style="background-image: url('${item.image}')"></figure>
           <div class="block-4-text p-4">
             <h3><a href="#">${item.name}</a></h3>
             <p class="mb-0">${item.farm}</p>
@@ -16,5 +30,32 @@ if ($('.group-purchase').length) {
         </div>
       </div>`).appendTo('.group-purchase')
     })
+
+    let slider = function() {
+      $('.group-purchase').owlCarousel({
+        center: false,
+        items: 1,
+        loop: false,
+        stagePadding: 15,
+        margin: 20,
+        nav: true,
+        navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+        responsive:{
+          600:{
+            margin: 20,
+            items: 2
+          },
+          1000:{
+            margin: 20,
+            items: 3
+          },
+          1200:{
+            margin: 20,
+            items: 3
+          }
+        }
+      });
+    };
+    slider();
   })
 }
