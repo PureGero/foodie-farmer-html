241fffd31e7bf7281c15df40b0637865374b0b09
尹杰亮 <yinjieliang@aol.com>
2020-10-08 11:17:49 +1000

    Add new produce page

--- foodie farmers/js/foodiefarmer.js
+++ foodie farmers/js/foodiefarmer.js
@@ -45,6 +45,28 @@ if ($('.recommend-items').length) {
   })
 }
 
+
+
+// Load produce items
+if ($('.produce-items').length) {
+  $.get('/api/customer/get_produce', data => {
+    data.forEach(item => {
+      $(`<div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
+        <div class="block-4 text-center border">
+          <figure class="block-4-image">
+            <a href="shop-single.html"><img src="${item.image}" alt="Image placeholder" class="img-fluid"></a>
+          </figure>
+          <div class="block-4-text p-4">
+            <h3><a href="shop-single.html">${item.name}</a></h3>
+            <p class="mb-0">${item.description}</p>
+            <p class="text-primary font-weight-bold">$${item.price}</p>
+          </div>
+        </div>
+      </div> `).appendTo('.produce-items')
+    })
+  })
+}
+
 // Load group purchases
 if ($('.group-purchase').length) {
   $.get('/api/customer/list_group_purchases', data => {
