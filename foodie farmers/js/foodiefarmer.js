349c4c543930462667ff1cc4abd31c8d350cfaaf
尹杰亮 <yinjieliang@aol.com>
2020-10-08 11:40:18 +1000

    Edit Produce Page

--- foodie farmers/js/foodiefarmer.js
+++ foodie farmers/js/foodiefarmer.js
@@ -54,7 +54,7 @@ if ($('.produce-items').length) {
       $(`<div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
         <div class="block-4 text-center border">
           <figure class="block-4-image">
-            <a href="shop-single.html"><img src="${item.image}" alt="Image placeholder" class="img-fluid"></a>
+            <a href="shop-single.html"><img src="${item.picture}" alt="Image placeholder" class="img-fluid"></a>
           </figure>
           <div class="block-4-text p-4">
             <h3><a href="shop-single.html">${item.name}</a></h3>
